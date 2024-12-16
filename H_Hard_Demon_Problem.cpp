d7a256120af1aa2337467207b9cfe64ba4ca9e4a
Satvik Mittal <satvikmittal638@gmail.com>
2024-12-16 11:51:55 +0530

    rectified problem-H

--- H_Hard_Demon_Problem.cpp
+++ H_Hard_Demon_Problem.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main()
 {
+    ios::sync_with_stdio(false);
+  cin.tie(nullptr);
     int tt;cin>>tt;
     while(tt--){
         int n,q;cin>>n>>q;
@@ -23,9 +25,8 @@ int main()
             long long sumiMij=p_iMij[x2][y2]-p_iMij[x1-1][y2]-p_iMij[x2][y1-1]+p_iMij[x1-1][y1-1],
                     sumjMij=p_jMij[x2][y2]-p_jMij[x1-1][y2]-p_jMij[x2][y1-1]+p_jMij[x1-1][y1-1],
                     sumMij=p_Mij[x2][y2]-p_Mij[x1-1][y2]-p_Mij[x2][y1-1]+p_Mij[x1-1][y1-1];
-            // TODO:form double summation again
             long long w=y2-y1+1;
-            long long ans=w*sumiMij + sumjMij - sumMij*((x1-1)*w+(y1-1)-1);
+            long long ans=w*sumiMij-(x1*w+(y1-1))*sumMij + sumjMij;
             cout<<ans<<" ";
         }
         cout<<"\n";
