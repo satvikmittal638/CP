e5bb792b1530e8055c00ed9c090d81d9e4c2df6b
Satvik Mittal <satvikmittal638@gmail.com>
2024-12-20 12:45:09 +0530

    Solved global round 28 D problem by Jiangly's code

--- D_Kevin_and_Competition_Memories.cpp
+++ D_Kevin_and_Competition_Memories.cpp
@@ -4,32 +4,37 @@ using namespace std;
 #define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define rall(x) x.rbegin(),x.rend()
 #define all(x) x.begin(),x.end()
+constexpr int inf=1e9;
+
 int main()
 {
 fastio;
     int tt;cin>>tt;
     while(tt--){
-       int n,m;cin>>n>>m;
-       vector<int> a(n),b(m);
-       for(auto &i:a) cin>>i;
-       for(auto &i:b) cin>>i;
-       sort(all(b));
-       vector<int> maxm_prob_solved(n);
-       for(int i=0;i<n;i++){
-        int ind=(upper_bound(all(b),a[i]))-b.begin();
-        maxm_prob_solved[i]=ind;
-       }
-       int cnt_better_players=0;
-       for(int i=1;i<n;i++){
-        if(maxm_prob_solved[i]>maxm_prob_solved[0]) cnt_better_players++;
-       }
-       vector<int> ans(m,0);
-       int j=maxm_prob_solved[0];
-       for(int k=1;k<=m;k++){
-        ans[k-1]=j/k + (m/k - j/k)*(cnt_better_players+1);
-       }
-       for(auto e:ans) cout<<e<<" ";
-       cout<<"\n";
+        int n,m;cin>>n>>m;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            a[i]=max(a[i],a[0]); // remove all players less rated than Kevin(a[0])
+        }
+        sort(all(a));
+        vector<int> b(m);
+        for(int i=0;i<m;i++){
+            cin>>b[i];
+            if(b[i]<=a[0]) b[i]=inf+1; // remove problems which can be solved by a[0] since they don't decide his rank
+        }
+        sort(rall(b));
+        // update b[i] with number of players who could solve the ith problem
+        for(int i=0;i<m;i++){
+            b[i]=a.end()-lower_bound(all(a),b[i]);
+        }
+        for(int k=1;k<=m;k++){
+            ll ans=0ll;
+            for(int i=k-1;i<m;i+=k){
+                ans+=b[i]+1; // rank(a[0])=(1+ the number of contestants who solve at least one problem in that competition )
+            }
+            cout<<ans<<" \n"[k==m]; // prints \n after the last iteration and a space otherwise
+        }
     }
     return 0;
 }
